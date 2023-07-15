6fd097c819e4e887f89582dc2baf23689174c985
Your Name <you@example.com>
2023-07-15 22:01:22 +0100

    m

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,12 +1,7 @@
--- Script creates a a table users following these requirements:
--- With these attributes:
--- id, integer, never null, auto increment and primary key
--- email, string (255 characters), never null and unique
--- name, string (255 characters)
--- If the table already exists, your script should not fail
-
+-- Creates users table.
 CREATE TABLE IF NOT EXISTS users (
-id INTEGER PRIMARY KEY AUTO_INCREMENT,
-email VARCHAR(255) NOT NULL UNIQUE,
-name VARCHAR(255)
+    id INT NOT NULL AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    PRIMARY KEY (id)
 );
--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,13 +1,8 @@
--- Script creates a a table users following these requirements:
--- With these attributes:
--- id, integer, never null, auto increment and primary key
--- email, string (255 characters), never null and unique
--- name, string (255 characters)
--- If the table already exists, your script should not fail
-
+-- creates a table users
 CREATE TABLE IF NOT EXISTS users (
-id INTEGER PRIMARY KEY AUTO_INCREMENT,
-email VARCHAR(255) NOT NULL UNIQUE,
-name VARCHAR(255),
-country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+    id INT NOT NULL AUTO_INCREMENT, 
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL,
+    PRIMARY KEY (id)
 );
